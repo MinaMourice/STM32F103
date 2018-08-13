5555c77791bb453e301ce3cf96828cdcb0f454af
Mina Mourice <minamourice11@yahoo.com>
2018-08-13 14:30:44 +0200

    NVIC Driver

---
+++ MCAL/MNVK/MNVK_interface.h
@@ -0,0 +1,24 @@
+/****************************************/
+/* Author	: Mina Mourice				*/
+/* Date		: 11 August 2018				*/
+/* Version	: V01						*/
+/****************************************/ 
+#ifndef _MNVK_INTERFACE_H_
+#define _MNVK_INTERFACE_H_
+
+#define MNVK_EXTI0 6 
+#define MNVK_EXTI1 7
+
+/* Enable MNVK */
+void MNVK_Enable (u8 ID);
+
+/* Disable MNVK */
+void MNVK_Disable (u8 ID);
+
+/* Clear Pending Flag */
+void MNVK_ClearPending (u8 ID);
+
+/* Set Pending Flag */
+void MNVK_SetPending (u8 ID);
+
+#endif
---
+++ MCAL/MNVK/MNVK_private.h
@@ -0,0 +1,18 @@
+/****************************************/
+/* Author	: Mina Mourice				*/
+/* Date		: 11 August 2018			*/
+/* Version	: V01						*/
+/****************************************/
+#ifndef _MNVK_PRIVATE_H_
+#define _MNVK_PRIVATE_H_
+
+
+#define MNVK_ISER ((volatile u32*)0xE000E100)
+
+#define MNVK_ICER ((volatile u32*)0xE000E180)
+
+#define MNVK_ISPR ((volatile u32*)0xE000E200)
+
+#define MNVK_ICPR ((volatile u32*)0xE000E280)
+
+#endif
